e47b8dcec03765f04431cb8c11618e08112c6918
patiphan2000 <patiphan.sri@ku.th>
2021-11-07 11:10:34 +0700

    create login page with firebase auth

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
-import './App.css';
-
+import React, { Component } from 'react'
 import LoginForm from './components/LoginForm'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
+class App extends Component {
+
+  render() {
+
+    console.log(process.env);
+    return (
+      <div className="App">
         <LoginForm />
-    </div>
-  );
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,25 +1,107 @@
 import React from 'react'
+import auth from '../firebase'
 
 class LoginForm extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      email: '',
+      password: '',
+      currentUser: null,
+      message: ''
+    }
+  }
+
+  onChange = e => {
+    const { name, value } = e.target
+
+    this.setState({
+      [name]: value
+    })
+  }
+
+  onSubmit = e => {
+    e.preventDefault()
+
+    const { email, password } = this.state
+    
+    auth
+      .signInWithEmailAndPassword(email, password)
+      .then(response => {
+        this.setState({
+          currentUser: response.user
+        })
+      })
+      .catch(error => {
+        this.setState({
+          message: error.message
+        })
+      })
+  }
+
+  componentDidMount() {
+    auth.onAuthStateChanged(user => {
+      if (user) {
+        this.setState({
+          currentUser: user
+        })
+      }
+    })
+  }
+
+  logout = (e) => {
+    e.preventDefault()
+    auth.signOut().then(response => {
+      this.setState({
+        currentUser: null
+      })
+    })
+  }
+
   render() {
+
+    const { message, currentUser } = this.state
+
+    if (currentUser) {
+      return (
+        <div>
+          <p>Hello {currentUser.email}</p>
+          <button onClick={this.logout}>Logout</button>
+        </div>
+      )
+    }
+
     return (
       <section className="section container">
         <div className="columns is-centered">
           <div className="column is-half">
-            <form>
+            <form onSubmit={this.onSubmit}>
               <div className="field">
                 <label className="label">Email</label>
                 <div className="control">
-                  <input className="input" type="email" name="email" />
+                  <input
+                    className="input"
+                    type="email"
+                    name="email"
+                    onChange={this.onChange}
+                  />
                 </div>
               </div>
 
               <div className="field">
                 <label className="label">Password</label>
                 <div className="control">
-                  <input className="input" type="password" name="password" />
+                  <input
+                    className="input"
+                    type="password"
+                    name="password"
+                    onChange={this.onChange}
+                  />
                 </div>
               </div>
+
+              {message ? <p className="help is-danger">{message}</p> : null}
 
               <div className="field is-grouped">
                 <div className="control">
