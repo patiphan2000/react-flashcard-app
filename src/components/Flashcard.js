18f14eaab440a3751e7ce7ee915b6637bc438df8
patiphan2000 <patiphan.sri@ku.th>
2021-11-08 12:42:57 +0700

    flash card with flip and button

--- src/components/Flashcard.js
+++ src/components/Flashcard.js
@@ -1,9 +1,46 @@
-import React from 'react'
+import React, {useState} from 'react'
+import './Flashcard.css'
 
+import Typography from '@mui/material/Typography';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import IconButton from '@mui/material/IconButton';
+import AutorenewIcon from '@mui/icons-material/Autorenew';
 
-function Flashcard() {
+function Flashcard({ flashcard }) {
+
+    const [flip, setFlip] = useState(false)
+
     return (
-    <h3>card</h3>
+        <div className={`flashcard ${flip? 'flip':''}`}>
+            <div className="front">
+                <Card>
+                    <CardContent>
+                        <Typography variant="h6">{flashcard.front.text}</Typography>
+                    </CardContent>
+                    <CardActions style={{justifyContent: 'center'}}>
+                        <IconButton aria-label="autorenewIcon" size="large"
+                        onClick={()=>{setFlip(!flip)}}>
+                            <AutorenewIcon fontSize="inherit" />
+                        </IconButton>
+                    </CardActions>
+                </Card>
+                </div>
+            <div className="back">
+                <Card>
+                    <CardContent>
+                        <Typography variant="h6">{flashcard.back.text}</Typography>
+                    </CardContent>
+                    <CardActions style={{justifyContent: 'center'}}>
+                        <IconButton aria-label="autorenewIcon" size="large"
+                        onClick={()=>{setFlip(!flip)}}>
+                            <AutorenewIcon fontSize="inherit" />
+                        </IconButton>
+                    </CardActions>
+                </Card>
+            </div>
+        </div>
     )
 }
 
