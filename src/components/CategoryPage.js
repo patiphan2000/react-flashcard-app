50d67d9356a2c941ae7789a2f815c4d15c5ddebd
patiphan2000 <patiphan.sri@ku.th>
2021-11-12 21:54:13 +0700

    fix categorypage position

--- src/components/CategoryPage.js
+++ src/components/CategoryPage.js
@@ -36,24 +36,26 @@ function CategoryPage ({}) {
         alignItems="center"
         justifyContent="center"
         sx={{ marginTop: "50px" }}>
-            <Grid container justifyContent="center" spacing={2}>
+            <Grid container spacing={2}>
                 {categorys.map((cat, index) => {
                     return (
                         <Grid key={index} item xs={12} sm={6}>
-                            <Card sx={{ maxWidth: 400 }}>
-                                <CardActionArea component={Link} to={"/category/"+cat.name}>
-                                    <CardMedia
-                                    component="img"
-                                    height="140"
-                                    alt="green iguana"
-                                    />
-                                    <CardContent>
-                                    <Typography gutterBottom variant="h5" component="div">
-                                        {cat.name}
-                                    </Typography>
-                                    </CardContent>
-                                </CardActionArea>
-                            </Card>
+                            <Grid container alignItems="center" justifyContent="center">
+                                <Card sx={{ width: {xs: 300, md: 400} }}>
+                                    <CardActionArea component={Link} to={"/category/"+cat.name}>
+                                        <CardMedia
+                                        component="img"
+                                        height="140"
+                                        alt="green iguana"
+                                        />
+                                        <CardContent>
+                                        <Typography gutterBottom variant="h5" component="div">
+                                            {cat.name}
+                                        </Typography>
+                                        </CardContent>
+                                    </CardActionArea>
+                                </Card>
+                            </Grid>
                         </Grid>
                     )
                 })}
