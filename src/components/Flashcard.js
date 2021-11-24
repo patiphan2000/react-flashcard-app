639dec3adbe24a410cd9b0ae9a3243370b4c0590
patiphan2000 <patiphan.sri@ku.th>
2021-11-24 19:25:23 +0700

    add polka dot to indicate flashcard side

--- src/components/Flashcard.js
+++ src/components/Flashcard.js
@@ -7,6 +7,7 @@ import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import IconButton from '@mui/material/IconButton';
 import AutorenewIcon from '@mui/icons-material/Autorenew';
+import { Box } from '@mui/system';
 
 function Flashcard({ flashcard, flip, setFlip }) {
 
@@ -21,44 +22,70 @@ function Flashcard({ flashcard, flip, setFlip }) {
         setHeight(Math.max(frontHeight, backHeight, 250))
     }
 
+    const polkaDot = (num1, num2, size) => {
+        return (
+            <>
+                <Box className={`circle ${flip? 'flip':''}`} sx={{ 
+                    background: '#b2a429',
+                    top: num1 + "%", 
+                    left: num2 + "%",
+                    width: {xs: size + "rem", sm: (size+3) + "rem"}, 
+                    height: {xs: size + "rem", sm: (size+3) + "rem"} }}></Box>
+                <Box className={`circle ${!flip? 'flip':''}`} sx={{ 
+                    background: '#a2cf6e',
+                    top: num1 + "%",
+                    left: num2 + "%", 
+                    width: {xs: size + "rem", sm: (size+3) + "rem"}, 
+                    height: {xs: size + "rem", sm: (size+3) + "rem"} }}></Box>
+            </>
+        )
+    }
+
     useEffect(() => {
         setMaxHeight()
     }, [flashcard.text, flashcard.subText])
 
     return (
-        <div className={`flashcard ${flip? 'flip':''}`} style={{height: height}}>
-            <div className="front" ref={frontEl}>
-                <Card sx={{ backgroundColor: '#fff8e7' }} onClick={()=>{setFlip(!flip)}}>
-                    <CardContent>
-                        <Typography className="text" variant="h5" sx={{ color: 'black' }}>{flashcard.front.text}</Typography>
-                    </CardContent>
-                    <CardContent>
-                        <Typography className="subText" variant="subtitle2" sx={{ color: '#656565' }}>{flashcard.front.subText}</Typography>
-                    </CardContent>
-                    <CardActions style={{justifyContent: 'center'}}>
-                        <IconButton aria-label="autorenewIcon" size="large" sx={{ color: '#656565' }}
-                        onClick={()=>{setFlip(!flip)}}>
-                            <AutorenewIcon fontSize="inherit" />
-                        </IconButton>
-                    </CardActions>
-                </Card>
+        <div>
+            <div className={`flashcard ${flip? 'flip':''}`} style={{height: height}}>
+                <div className="front" ref={frontEl}>
+                    <Card sx={{ backgroundColor: '#fff8e7' }} onClick={()=>{setFlip(!flip)}}>
+                        <CardContent>
+                            <Typography className="text" variant="h5" sx={{ color: 'black' }}>{flashcard.front.text}</Typography>
+                        </CardContent>
+                        <CardContent>
+                            <Typography className="subText" variant="subtitle2" sx={{ color: '#656565' }}>{flashcard.front.subText}</Typography>
+                        </CardContent>
+                        <CardActions style={{justifyContent: 'center'}}>
+                            <IconButton aria-label="autorenewIcon" size="large" sx={{ color: '#656565' }}
+                            onClick={()=>{setFlip(!flip)}}>
+                                <AutorenewIcon fontSize="inherit" />
+                            </IconButton>
+                        </CardActions>
+                    </Card>
+                    </div>
+                <div className="back" ref={backEl}>
+                    <Card sx={{ backgroundColor: '#fff8e7' }} onClick={()=>{setFlip(!flip)}}>
+                        <CardContent>
+                            <Typography className="text" variant="h5" sx={{ color: 'black' }}>{flashcard.back.text}</Typography>
+                        </CardContent>
+                        <CardContent>
+                            <Typography className="subText" variant="subtitle2" sx={{ color: '#656565' }}>{flashcard.back.subText}</Typography>
+                        </CardContent>
+                        <CardActions style={{justifyContent: 'center'}}>
+                            <IconButton aria-label="autorenewIcon" size="large" sx={{ color: '#656565' }}
+                            onClick={()=>{setFlip(!flip)}}>
+                                <AutorenewIcon fontSize="inherit" />
+                            </IconButton>
+                        </CardActions>
+                    </Card>
                 </div>
-            <div className="back" ref={backEl}>
-                <Card sx={{ backgroundColor: '#fff8e7' }} onClick={()=>{setFlip(!flip)}}>
-                    <CardContent>
-                        <Typography className="text" variant="h5" sx={{ color: 'black' }}>{flashcard.back.text}</Typography>
-                    </CardContent>
-                    <CardContent>
-                        <Typography className="subText" variant="subtitle2" sx={{ color: '#656565' }}>{flashcard.back.subText}</Typography>
-                    </CardContent>
-                    <CardActions style={{justifyContent: 'center'}}>
-                        <IconButton aria-label="autorenewIcon" size="large" sx={{ color: '#656565' }}
-                        onClick={()=>{setFlip(!flip)}}>
-                            <AutorenewIcon fontSize="inherit" />
-                        </IconButton>
-                    </CardActions>
-                </Card>
             </div>
+            {polkaDot(10, 50, 22)}
+            {polkaDot(5, 18, 5)}
+            {polkaDot(42, 18, 12)}
+            {polkaDot(12, 82, 12)}
+            {polkaDot(60, 75, 5)}
         </div>
     )
 }
